462521f152867a818de57d70dfaffeee5a1032bc
Dioum <kd291030@c42114l.ens.uqam.ca>
2020-01-24 15:28:14 -0500

    modification du programme

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,7 +3,7 @@ package labo2;
 public class FizzBuzz {
 public static void main(String [] args) {
 	
-	for(int i=1; i<= 100 ; i++) {
+	for(int i=1; i<= 200 ; i++) {
 		if(i%3==0) {
 			System.out.println("Fizz");
 		}else if(i%5==0) {
