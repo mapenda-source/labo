d187b6e09144f420fc5c73645658e3e93435cd6e
Mapenda Dioum <mapendadioum3@gmail.com>
2020-01-24 14:37:58 -0500

    programme FizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,18 @@
+package labo2;
+
+public class FizzBuzz {
+public static void main(String [] args) {
+	
+	for(int i=1; i<= 100 ; i++) {
+		if(i%3==0) {
+			System.out.println("Fizz");
+		}else if(i%5==0) {
+			System.out.println("Bizz");
+			}
+			else if(i%3==0 && i%5==0 ) {
+				System.out.println("FizzBuzz");
+			}
+		}
+	}
+}
+
